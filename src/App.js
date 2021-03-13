0b8f963e6a9906a96d3d2a79117a8352b526e951
Krutarth126 <krutarthgupta1206@gmail.com>
2021-03-13 15:28:45 +0530

    img

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Nav from "./component/navbar";
 
 function App() {
+  const [imagerender, updateimgrender] = useState("");
+
+
+  function renderimages(responseData) {
+    const imagearray = responseData.map((imgobj, index) => {
+      const imgurl = imgobj.download_url;
+      const author = imgobj.author;
+      console.log(index)
+
+      function showimg(elem, imgsrc){
+        let yash = elem
+        let source= imgsrc 
+        var modal = document.getElementById("myModal");
+        var img = document.getElementById(yash);
+        var modalImg = document.getElementById("img01");
+        modal.style.display = "block";
+        modalImg.src = source;
+        
+      }
+
+      function close(){
+        var modal = document.getElementById("myModal");
+        modal.style.display = "none";
+      }
+      
+      return (
+        
+       <>
+        <div className="gallery">
+          <img id={index} src={imgurl} width="200" id={index} onClick={()=> showimg(index, imgurl)} />
+          <div className="author">This image is captured by {author}</div>
+          </div>
+          <div id="myModal" class="modal">
+            <span class="close" onClick={close}>&times;</span>
+            <img class="modal-content" id="img01"/>
+          </div> 
+        </> 
+      );
+    });
+    updateimgrender(imagearray);
+  }
+
+  async function getData() {
+    let data = await axios("https://picsum.photos/v2/list");
+    console.log(data.data);
+    renderimages(data.data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+  // popup();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <div className="App">{imagerender}</div>
+    </>
   );
 }
 
---
+++ src/component/navbar.js
@@ -0,0 +1,10 @@
+import "../App.css"
+function Nav(){
+    return(
+        <div className="navbar">
+            <h1>Image gallery</h1>
+        </div>
+    )
+}
+
+export default Nav;
